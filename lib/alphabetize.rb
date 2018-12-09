50f5e2584230afebca2827ffdf9b57005c0d9bde
Anderson Coelho <anderson-tomas@hotmail.com>
2018-12-09 21:32:47 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,5 @@
 require 'pry'
 def alphabetize(arr)
   # code here
-  arr.sort_by do |sentence|
-  sentence.split("").map do |letter|
-    ALPHABET.index(letter)
-
-  end
-end
+  arr.sort_by {|word| word.split("").map {|letter| ALPHABET.index(letter)}}
 end
